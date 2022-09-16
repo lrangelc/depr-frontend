cd0dc670acad78fb4b9fc37f32e810496bd67a96
Luis Rangel Castro <luisrangelc@gmail.com>
2022-09-15 20:57:22 -0600

    feat: add modules

---
+++ src/app/banking-transactions/banking-transactions.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BankingTransactionsRoutingModule } from './banking-transactions-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BankingTransactionsRoutingModule
+  ]
+})
+export class BankingTransactionsModule { }
